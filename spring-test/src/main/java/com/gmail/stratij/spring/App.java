d5a85d56630fbce1c80dfdf668b1062cc700048d
Katran91 <stratij@gmail.com>
2015-02-12 17:51:03 +0200

    Init base configuration

---
+++ spring-test/src/main/java/com/gmail/stratij/spring/App.java
@@ -0,0 +1,13 @@
+package com.gmail.stratij.spring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
