d5d9727c72fe951bba1122196dcfc9d127dcb504
Katran91 <stratij@gmail.com>
2015-02-12 18:52:08 +0200

    Add bean

--- spring-test/src/main/java/com/gmail/stratij/spring/App.java
+++ spring-test/src/main/java/com/gmail/stratij/spring/App.java
@@ -1,5 +1,9 @@
 package com.gmail.stratij.spring;
 
+import com.gmail.stratij.spring.sequence.SequenceGenerator;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
@@ -9,5 +13,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+        SequenceGenerator generator = (SequenceGenerator) context.getBean("sequenceGenerator");
     }
 }
